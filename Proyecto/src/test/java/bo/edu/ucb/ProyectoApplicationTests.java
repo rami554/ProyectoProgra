3ddd0396bf446623aa61f4ad07d52caf4bbfd494
rami554 <remydhi@gmail.com>
2020-05-23 16:05:08 -0400

    Creacion del proyecto

---
+++ Proyecto/src/test/java/bo/edu/ucb/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
