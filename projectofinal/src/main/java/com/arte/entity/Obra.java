f9b96c3c961ff21fd00774b1adfa404d9d99aec2
rami554 <remydhi@gmail.com>
2020-05-23 22:35:50 -0400

    problemas Resueltos

--- projectofinal/src/main/java/com/arte/entity/Obra.java
+++ projectofinal/src/main/java/com/arte/entity/Obra.java
@@ -15,38 +15,10 @@ public class Obra {
     private float precio;
     private int idArtista;
 
-<<<<<<< HEAD
-=======
     public Obra() {
 		super();
 	}
-<<<<<<< HEAD
-=======
-		// TODO Auto-generated constructor stub
-	}
-    
 
-	public Obra(int idObra, String titulo, String estilo, float precio, int idArtista) {
-		super();
-		this.idObra = idObra;
-		this.titulo = titulo;
-		this.estilo = estilo;
-		this.precio = precio;
-		this.idArtista = idArtista;
-	}
-
-
->>>>>>> d44bfa1957e4151bd98f5874b3511d8f89a279a0
-	@Id
-    @Column(name = "id_obra")
-    public int getIdObra() {
-        return idObra;
-    }
->>>>>>> 9f8577523b39b4c3524e34ff6cdc37cfa5193836
-
-=======
-   
->>>>>>> 7187d78430895a6203f0a8ee1d2448b6b5bd4ff2
 	public Obra(int idObra, String titulo, String estilo, float precio, int idArtista) {
 		super();
 		this.idObra = idObra;
