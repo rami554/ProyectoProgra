f3d8b7f33f94fe153903e5213cb51a25f6c05727
juan512 <juandanielpcutile.jc@gmail.com>
2020-05-23 21:16:34 -0400

    Correccion de Servicios

--- projectofinal/src/main/java/com/arte/controllers/ExposicionController.java
+++ projectofinal/src/main/java/com/arte/controllers/ExposicionController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.arte.services.ExposicionService;
+
 @RestController
 @RequestMapping("/Persona")
 
@@ -11,3 +13,4 @@ public class ExposicionController {
 
 	@Autowired
 	private ExposicionService exposicionService;
+}
